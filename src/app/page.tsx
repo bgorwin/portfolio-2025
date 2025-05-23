9acca0e5ca6c3d4d216be3c376cfe71c75a0e6eb
Brent Gorwin <brent.gorwin@gmail.com>
2025-05-23 11:45:26 -0700

    pushing new project to github repo

---
+++ src/app/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import Image from "next/image";
+import { gsap } from "gsap";
+import { useEffect } from "react";
+
+
+  
+
+export default function Home() {
+  useEffect(() => {
+    gsap.from(".heading-1", {
+      y: 100,         // start 50px below
+      opacity: 0,    // start transparent
+      duration: 2,   // animation lasts 2 second
+    });
+  });
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="flex flex-col md:flex-row lg:flex-row gap-[32px] justify-start">
+          <div className="flex flex-col flex-3/4 gap-[8px]">
+            <h1 className="heading-1 text-9xl font-bold text-white">This is Brents Profile</h1>
+          </div>
+          <div className="flex flex-col flex-1/4 justify-items-center gap-[8px] h-full">
+            <p className="text-2xl text-gray-400">Software Engineer</p>
+          </div>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
